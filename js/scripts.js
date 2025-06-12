93ea2cad9b1b3bdb4d827dfb10e98b39b9ff16fc
Mrinalini29 <m.bhatnagar2909@gmail.com>
2025-06-11 22:29:06 -0500

    JavaScript Libraries & Plugins

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 
   alert("Hello");
 console.log("Hello! Welcome to my website. I am Mrinalini a UX/UI designer.")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
